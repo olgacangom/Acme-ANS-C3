d8891a77951a7078f0f4596107ed3721881c438a
Alberto <albescsan1@alum.us.es>
2025-05-24 13:22:07 +0200

    first student1 tests done

---
+++ src/main/java/acme/entities/booking/FlightRepository.java
@@ -0,0 +1,27 @@
+/*
+ * AnyLegRepository.java
+ *
+ * Copyright (C) 2012-2025 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.entities.booking;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.components.datatypes.Money;
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface FlightRepository extends AbstractRepository {
+
+	@Query("select f.cost from Flight f where f.id = :flightId")
+	Money findCostByFlight(int flightId);
+
+}
